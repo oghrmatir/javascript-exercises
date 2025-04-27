58b614b084582c9bcc87b19919c23f5b5ae013d6
Argho Maitra <207898299+oghrmatir@users.noreply.github.com>
2025-04-28 00:28:45 +0530

    Solve exercises in the "Array and Loops" lesson

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringToReturn = string.split("").reverse().join("");
+  return stringToReturn;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  let total = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
